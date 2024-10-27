5fb8f30b5e8f351f21dd87cf2a8534c1217eccf9
Abid ali <khanali3229>
2024-10-27 10:19:20 +0500

    after puching project i working with looping for learning the js and also pass the coding challenge

--- script.js
+++ script.js
@@ -80,6 +80,54 @@ const displayMovements = function (movements) {
 };
 
 displayMovements(account1.movements);
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
+
+Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
+
+1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats, not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
+2. Create an array with both Julia's (corrected) and Kate's data
+3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 🐶")
+4. Run the function for both test datasets
+
+HINT: Use tools from all lectures in this section so far 😉
+
+TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+
+
+const checkDogs = function (JuliaDogs, KateDogs) {
+  const shallowCopy = JuliaDogs.slice();
+  shallowCopy.splice(0, 1);
+  shallowCopy.splice(-2);
+  console.log(shallowCopy);
+
+  const dogData = shallowCopy.concat(KateDogs);
+  console.log(dogData);
+
+  // return shallowCopy;
+
+  dogData.forEach(function (dogs, i, arr) {
+    if (dogs >= 3) {
+      console.log(
+        `Dog number ${i + 1} is in adult,and is ${dogs} years old 🐕`
+      );
+    } else {
+      console.log(`Dog number ${dogs} is still a puppy 🐶`);
+    }
+  });
+};
+checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+*/
+// ("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 🐶")
+// console.log(...dogData);
+// console.log(...result);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -180,3 +228,24 @@ currenciesUnique.forEach(function (value, key, map) {
   console.log(`${key}: ${value}`);
 });
 */
+
+////////////////////////////
+////MAP////
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const eurToUsd = 1.1;
+
+const changeCurrency = movements.map(mov => {
+  return Math.trunc(mov * eurToUsd);
+});
+console.log(movements);
+console.log(changeCurrency);
+
+const movementDescription = movements.map(
+  (mov, i) =>
+    `movment ${i + 1}: you ${mov > 0 ? 'defosited' : 'withdrew'} ${Math.abs(
+      mov
+    )}`
+);
+console.log(movementDescription);
