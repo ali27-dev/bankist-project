6cbdc2ad07b67d0aeb942bae1488907b1c4d7f7b
Abid ali <khanali3229>
2024-11-03 10:15:32 +0500

    today the topic is,i learn all methods but which one is used lets do practice on it and thire have a coding challenge

--- script.js
+++ script.js
@@ -234,66 +234,9 @@ btnSort.addEventListener('click', function (e) {
 // LECTURES
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-///////////////////////////////////////
-// Coding Challenge #1
 
-/* 
-Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
-
-Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
-
-1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats, not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
-2. Create an array with both Julia's (corrected) and Kate's data
-3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 🐶")
-4. Run the function for both test datasets
-
-HINT: Use tools from all lectures in this section so far 😉
-
-TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
-TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
-
-GOOD LUCK 😀
-
-
-const checkDogs = function (JuliaDogs, KateDogs) {
-  const shallowCopy = JuliaDogs.slice();
-  shallowCopy.splice(0, 1);
-  shallowCopy.splice(-2);
-  console.log(shallowCopy);
-
-  const dogData = shallowCopy.concat(KateDogs);
-  console.log(dogData);
-
-  // return shallowCopy;
-
-  dogData.forEach(function (dogs, i, arr) {
-    if (dogs >= 3) {
-      console.log(
-        `Dog number ${i + 1} is in adult,and is ${dogs} years old 🐕`
-      );
-    } else {
-      console.log(`Dog number ${dogs} is still a puppy 🐶`);
-    }
-  });
-};
-checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
-*/
-// ("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 🐶")
-// console.log(...dogData);
-// console.log(...result);
-
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
-
-// const currencies = new Map([
-//   ['USD', 'United States dollar'],
-//   ['EUR', 'Euro'],
-//   ['GBP', 'Pound sterling'],
-// ]);
-
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 /*
+/////////////////////////////////////////////////
 // Simple Array Methods
 let arr = ['a', 'b', 'c', 'd', 'e'];
 
@@ -347,24 +290,32 @@ console.log('jonas'.at(-1));
 // Looping Arrays: forEach
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
+// for (const movement of movements) {
 for (const [i, movement] of movements.entries()) {
   if (movement > 0) {
-    console.log(`Movement ${i + 1}: you deposited ${movement}`);
+    console.log(`Movement ${i + 1}: You deposited ${movement}`);
   } else {
-    console.log(`Movement ${i + 1}: you withderw ${Math.abs(movement)}`);
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(movement)}`);
   }
 }
-console.log('------------ FOREACH -------');
 
-movements.forEach(function (movement, i, arr) {
-  if (movement > 0) {
-    console.log(`Movement ${i + 1}: you deposited ${movement}`);
+console.log('---- FOREACH ----');
+movements.forEach(function (mov, i, arr) {
+  if (mov > 0) {
+    console.log(`Movement ${i + 1}: You deposited ${mov}`);
   } else {
-    console.log(`Movement ${i + 1}: you withderw ${Math.abs(movement)}`);
+    console.log(`Movement ${i + 1}: You withdrew ${Math.abs(mov)}`);
   }
 });
+// 0: function(200)
+// 1: function(450)
+// 2: function(400)
+// ...
 
-// MAP
+
+///////////////////////////////////////
+// forEach With Maps and Sets
+// Map
 const currencies = new Map([
   ['USD', 'United States dollar'],
   ['EUR', 'Euro'],
@@ -375,64 +326,121 @@ currencies.forEach(function (value, key, map) {
   console.log(`${key}: ${value}`);
 });
 
-// SET
-const currenciesUnique = new Set(['USD', 'USD', 'URE', 'PKR', 'AED', 'PKR']);
-
-currenciesUnique.forEach(function (value, key, map) {
-  console.log(`${key}: ${value}`);
+// Set
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+console.log(currenciesUnique);
+currenciesUnique.forEach(function (value, _, map) {
+  console.log(`${value}: ${value}`);
 });
+*/
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
+
+Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
+
+1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats, not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
+2. Create an array with both Julia's (corrected) and Kate's data
+3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 🐶")
+4. Run the function for both test datasets
+
+HINT: Use tools from all lectures in this section so far 😉
+
+TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
+/*
+const checkDogs = function (dogsJulia, dogsKate) {
+  const dogsJuliaCorrected = dogsJulia.slice();
+  dogsJuliaCorrected.splice(0, 1);
+  dogsJuliaCorrected.splice(-2);
+  // dogsJulia.slice(1, 3);
+  const dogs = dogsJuliaCorrected.concat(dogsKate);
+  console.log(dogs);
+
+  dogs.forEach(function (dog, i) {
+    if (dog >= 3) {
+      console.log(`Dog number ${i + 1} is an adult, and is ${dog} years old`);
+    } else {
+      console.log(`Dog number ${i + 1} is still a puppy 🐶`);
+    }
+  });
+};
+// checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
 
 
-////////////////////////////
-////MAP////
-
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
+///////////////////////////////////////
+// The map Method
 const eurToUsd = 1.1;
 
-const changeCurrency = movements.map(mov => {
-  return Math.trunc(mov * eurToUsd);
-});
-console.log(movements);
-console.log(changeCurrency);
+// const movementsUSD = movements.map(function (mov) {
+//   return mov * eurToUsd;
+// });
 
-const movementDescription = movements.map(
+const movementsUSD = movements.map(mov => mov * eurToUsd);
+
+console.log(movements);
+console.log(movementsUSD);
+
+const movementsUSDfor = [];
+for (const mov of movements) movementsUSDfor.push(mov * eurToUsd);
+console.log(movementsUSDfor);
+
+const movementsDescriptions = movements.map(
   (mov, i) =>
-    `movment ${i + 1}: you ${mov > 0 ? 'defosited' : 'withdrew'} ${Math.abs(
+    `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
       mov
     )}`
 );
-console.log(movementDescription);
+console.log(movementsDescriptions);
 
-///////////////////////////////
-//////FILLTER/////
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-const withdrewl = [];
-for (const element of movements) if (element < 0) withdrewl.push(element);
-console.log(withdrewl);
-
-const deposite = movements.filter(function (mov) {
-  return mov < 0;
+///////////////////////////////////////
+// The filter Method
+const deposits = movements.filter(function (mov, i, arr) {
+  return mov > 0;
 });
+console.log(movements);
+console.log(deposits);
 
-console.log(deposite);
+const depositsFor = [];
+for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+console.log(depositsFor);
 
-////////////////////////////
-////REDUCE/////
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const withdrawals = movements.filter(mov => mov < 0);
+console.log(withdrawals);
 
-const balance = movements.reduce(function (acc, cur, i, arr) {
-  return acc + cur;
-});
+
+///////////////////////////////////////
+// The reduce Method
+console.log(movements);
+
+// accumulator -> SNOWBALL
+// const balance = movements.reduce(function (acc, cur, i, arr) {
+//   console.log(`Iteration ${i}: ${acc}`);
+//   return acc + cur;
+// }, 0);
+const balance = movements.reduce((acc, cur) => acc + cur, 0);
 console.log(balance);
 
+let balance2 = 0;
+for (const mov of movements) balance2 += mov;
+console.log(balance2);
 
-const max = movements.reduce((acc, mov, i, arr) => {
+// Maximum value
+const max = movements.reduce((acc, mov) => {
   if (acc > mov) return acc;
   else return mov;
 }, movements[0]);
 console.log(max);
+*/
 
 ///////////////////////////////////////
 // Coding Challenge #2
@@ -451,81 +459,226 @@ TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
 TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
 
 GOOD LUCK 😀
+*/
 
+/*
+const calcAverageHumanAge = function (ages) {
+  const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
+  const adults = humanAges.filter(age => age >= 18);
+  console.log(humanAges);
+  console.log(adults);
 
-const calcAverageHumanAge = function (dogAges) {
-  const ages = dogAges.map(mov => (mov <= 2 ? 2 * mov : 16 + mov * 4));
+  // const average = adults.reduce((acc, age) => acc + age, 0) / adults.length;
 
-  const adult = ages.filter(age => age >= 18);
+  const average = adults.reduce(
+    (acc, age, i, arr) => acc + age / arr.length,
+    0
+  );
 
-  const average = adult.reduce((acc, sum) => acc + sum, 0) / adult.length;
+  // 2 3. (2+3)/2 = 2.5 === 2/2+3/2 = 2.5
 
-  console.log(ages);
-  console.log(adult);
-  console.log(average);
   return average;
 };
-calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
-calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
-
-const calcAverageHumanAge2 = ages =>
-  ages
-    .map(mov => (mov <= 2 ? 2 * mov : 16 + mov * 4))
-    .filter(mov => mov >= 18)
-    .reduce((acc, mov, i, arr) => acc + mov / arr.length, 0);
-
-const avg1 = calcAverageHumanAge2([5, 2, 4, 1, 15, 8, 3]);
-const avg2 = calcAverageHumanAge2([16, 6, 10, 5, 6, 1, 4]);
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
 console.log(avg1, avg2);
 
+
+///////////////////////////////////////
+// The Magic of Chaining Methods
 const eurToUsd = 1.1;
 console.log(movements);
 
-const totalDeposite = movements
+// PIPELINE
+const totalDepositsUSD = movements
   .filter(mov => mov > 0)
   .map((mov, i, arr) => {
+    // console.log(arr);
     return mov * eurToUsd;
   })
+  // .map(mov => mov * eurToUsd)
   .reduce((acc, mov) => acc + mov, 0);
+console.log(totalDepositsUSD);
+*/
 
-console.log(totalDeposite);
+///////////////////////////////////////
+// Coding Challenge #3
 
-THE Find method\
+/* 
+Rewrite the 'calcAverageHumanAge' function from the previous challenge, but this time as an arrow function, and using chaining!
 
-const firstWithdrawel = movements.find(mov => mov < 0);
-console.log(firstWithdrawel);
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
+/*
+const calcAverageHumanAge = ages =>
+  ages
+    .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter(age => age >= 18)
+    .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+// adults.length
+
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+console.log(avg1, avg2);
 
 
+///////////////////////////////////////
+// The find Method
+const firstWithdrawal = movements.find(mov => mov < 0);
+console.log(movements);
+console.log(firstWithdrawal);
+
+console.log(accounts);
+
+const account = accounts.find(acc => acc.owner === 'Jessica Davis');
+console.log(account);
 
 
-//////////////////////
-///Flat and FlatMap///
+///////////////////////////////////////
+// some and every
+console.log(movements);
 
-const arr = [1, 3, 4, 54, 5, 6, 7, 7, 87, 8];
+// EQUALITY
+console.log(movements.includes(-130));
+
+// SOME: CONDITION
+console.log(movements.some(mov => mov === -130));
+
+const anyDeposits = movements.some(mov => mov > 0);
+console.log(anyDeposits);
+
+// EVERY
+console.log(movements.every(mov => mov > 0));
+console.log(account4.movements.every(mov => mov > 0));
+
+// Separate callback
+const deposit = mov => mov > 0;
+console.log(movements.some(deposit));
+console.log(movements.every(deposit));
+console.log(movements.filter(deposit));
+
+
+///////////////////////////////////////
+// flat and flatMap
+const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
 console.log(arr.flat());
 
-const accMovements = accounts;
-console.log(accMovements);
+const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
+console.log(arrDeep.flat(2));
 
-const newMovements = accMovements
+// flat
+const overalBalance = accounts
   .map(acc => acc.movements)
   .flat()
-  .reduce((acu, acc) => acu + acc, 0);
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overalBalance);
 
-console.log(newMovements);
+// flatMap
+const overalBalance2 = accounts
+  .flatMap(acc => acc.movements)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overalBalance2);
 
-/////////////////////////
-// Sorting Arrays//////////
 
-movements.sort((a, b) => {
-  if (a > b) return 1;
-  if (a < b) return -1;
-});
+///////////////////////////////////////
+// Sorting Arrays
+
+// Strings
+const owners = ['Jonas', 'Zach', 'Adam', 'Martha'];
+console.log(owners.sort());
+console.log(owners);
+
+// Numbers
 console.log(movements);
 
-movements.sort((a, b) => {
-  if (a > b) return -1;
-  if (a < b) return 1;
-});
+// return < 0, A, B (keep order)
+// return > 0, B, A (switch order)
+
+// Ascending
+// movements.sort((a, b) => {
+//   if (a > b) return 1;
+//   if (a < b) return -1;
+// });
+movements.sort((a, b) => a - b);
 console.log(movements);
+
+// Descending
+// movements.sort((a, b) => {
+//   if (a > b) return -1;
+//   if (a < b) return 1;
+// });
+movements.sort((a, b) => b - a);
+console.log(movements);
+
+
+///////////////////////////////////////
+// More Ways of Creating and Filling Arrays
+const arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+
+// Emprty arrays + fill method
+const x = new Array(7);
+console.log(x);
+// console.log(x.map(() => 5));
+x.fill(1, 3, 5);
+x.fill(1);
+console.log(x);
+
+arr.fill(23, 2, 6);
+console.log(arr);
+
+// Array.from
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y);
+
+const z = Array.from({ length: 7 }, (_, i) => i + 1);
+console.log(z);
+
+labelBalance.addEventListener('click', function () {
+  const movementsUI = Array.from(
+    document.querySelectorAll('.movements__value'),
+    el => Number(el.textContent.replace('€', ''))
+  );
+  console.log(movementsUI);
+
+  const movementsUI2 = [...document.querySelectorAll('.movements__value')];
+});
+*/
+
+///////////////////////////////////////
+// Array Methods Practice
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/* 
+Julia and Kate are still studying dogs, and this time they are studying if dogs are eating too much or too little.
+Eating too much means the dog's current food portion is larger than the recommended portion, and eating too little is the opposite.
+Eating an okay amount means the dog's current food portion is within a range 10% above and 10% below the recommended portion (see hint).
+
+1. Loop over the array containing dog objects, and for each dog, calculate the recommended food portion and add it to the object as a new property. Do NOT create a new array, simply loop over the array. Forumla: recommendedFood = weight ** 0.75 * 28. (The result is in grams of food, and the weight needs to be in kg)
+2. Find Sarah's dog and log to the console whether it's eating too much or too little. HINT: Some dogs have multiple owners, so you first need to find Sarah in the owners array, and so this one is a bit tricky (on purpose) 🤓
+3. Create an array containing all owners of dogs who eat too much ('ownersEatTooMuch') and an array with all owners of dogs who eat too little ('ownersEatTooLittle').
+4. Log a string to the console for each array created in 3., like this: "Matilda and Alice and Bob's dogs eat too much!" and "Sarah and John and Michael's dogs eat too little!"
+5. Log to the console whether there is any dog eating EXACTLY the amount of food that is recommended (just true or false)
+6. Log to the console whether there is any dog eating an OKAY amount of food (just true or false)
+7. Create an array containing the dogs that are eating an OKAY amount of food (try to reuse the condition used in 6.)
+8. Create a shallow copy of the dogs array and sort it by recommended food portion in an ascending order (keep in mind that the portions are inside the array's objects)
+
+HINT 1: Use many different tools to solve these challenges, you can use the summary lecture to choose between them 😉
+HINT 2: Being within a range 10% above and below the recommended portion means: current > (recommended * 0.90) && current < (recommended * 1.10). Basically, the current portion should be between 90% and 110% of the recommended portion.
+
+TEST DATA:
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] }
+];
+
+GOOD LUCK 😀
 */
