7e68bd6341b447ecbcc54a977e264ffeb486fb7b
Abid ali <khanali3229>
2024-11-04 08:58:23 +0500

    before  solveing the coding challege i do practice on four exercises

--- script.js
+++ script.js
@@ -668,6 +668,37 @@ const numDeposit = accounts
   .reduce((count, cur) => (cur >= 1000 ? ++count : count), 0);
 console.log(numDeposit);
 
+//3
+const { deposite, withdrewal } = accounts
+  .flatMap(mov => mov.movements)
+  .reduce(
+    (sums, curr) => {
+      sums[curr > 0 ? 'deposite' : 'withdrewal'] += curr;
+      return sums;
+    },
+    { deposite: 0, withdrewal: 0 }
+  );
+console.log(deposite, withdrewal);
+
+// 4.
+
+const convertTitleCase = title => {
+  const capitalize = str => str[0].toUpperCase() + str.slice(1);
+
+  const exception = ['a', 'an', 'and', 'the', 'but', 'or', 'on', 'in', 'with'];
+
+  const titleCase = title
+    .toLowerCase()
+    .split(' ')
+    .map(word => (exception.includes(word) ? word : capitalize(word)))
+    .join(' ');
+  return capitalize(titleCase);
+};
+
+console.log(convertTitleCase('this is a nice title'));
+console.log(convertTitleCase('this is a LONG title but not too long'));
+console.log(convertTitleCase('and here is another title with an EXAMPLE'));
+
 ///////////////////////////////////////
 // Coding Challenge #4
 
