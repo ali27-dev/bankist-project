a5597470050091896db414c54d096a35e0227d27
Abid ali <khanali3229>
2024-11-07 11:03:59 +0500

    next exercise i learn the remainder operator and also do practice on it

--- practice.js
+++ practice.js
@@ -601,7 +601,7 @@ console.log(Number.isInteger(23));
 console.log(Number.isInteger('20'));
 console.log(Number.isInteger(23.45));
 console.log(Number.isInteger(23));
-*/
+
 //////////////////
 //MATH an RANDOM
 
@@ -650,3 +650,30 @@ console.log((2.7).toFixed(0));
 console.log((2.7).toFixed(3));
 console.log((2.345).toFixed(2));
 console.log(+(2.7).toFixed(2));
+*/
+////////////////////////////////
+// remainder operator
+console.log(5 % 2);
+console.log(5 / 2);
+
+console.log(8 % 3);
+console.log(8 / 3);
+
+console.log(6 % 2);
+console.log(6 / 2);
+
+console.log(7 % 2);
+console.log(7 / 2);
+
+const isEven = n => n % 2 === 0;
+console.log(isEven(8));
+console.log(isEven(19));
+console.log(isEven(24));
+console.log(isEven(3284));
+
+labelBalance.addEventListener('click', function () {
+  [...document.querySelectorAll('.movements__row')].forEach(function (row, i) {
+    if (i % 2 === 0) row.style.backgroundColor = 'orangered';
+    if (i % 3 === 0) row.style.backgroundColor = 'green';
+  });
+});
