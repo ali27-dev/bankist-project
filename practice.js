42db01c035c2cb2c1b7965d760bc2918b6e1ca84
Abid ali <khanali3229>
2024-11-07 20:53:37 +0500

    i also practice with numaric operator and BigInt with examples

--- practice.js
+++ practice.js
@@ -650,7 +650,7 @@ console.log((2.7).toFixed(0));
 console.log((2.7).toFixed(3));
 console.log((2.345).toFixed(2));
 console.log(+(2.7).toFixed(2));
-*/
+
 ////////////////////////////////
 // remainder operator
 console.log(5 % 2);
@@ -677,3 +677,47 @@ labelBalance.addEventListener('click', function () {
     if (i % 3 === 0) row.style.backgroundColor = 'green';
   });
 });
+
+////////////////////////////
+///NUmeric separator//
+console.log(1_000_000);
+
+console.log(345_6543);
+
+const PI = 3.14_65;
+console.log(PI);
+
+console.log(Number('234_123'));
+console.log(parseInt('234_123'));
+
+// //////////////////////////
+///BigINt
+console.log(2 ** 53 - 1);
+console.log(Number.MAX_SAFE_INTEGER);
+console.log(2 ** 53 - 1);
+console.log(2 ** 53 - 2);
+console.log(2 ** 53 - 3);
+console.log(2 ** 53 - 4);
+
+console.log(34243453454645756768678765465323424n);
+console.log(123432443546464545343n * 32342342n);
+
+// operation
+console.log(10000n * 134234235345n);
+
+const huge = 23434524232423n;
+const num = 23;
+console.log(huge * BigInt(num));
+
+// Exceptions
+console.log(20n > 15);
+console.log(20n === 20);
+console.log(typeof 20n);
+console.log(20n == '20');
+
+console.log(huge + 'is REAllY big!!!');
+
+// Divisions
+console.log(11n / 3n);
+console.log(10 / 3);
+*/
